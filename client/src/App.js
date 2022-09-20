e6520cc901ce9973c2edf039217b172edf239bf1
aniketbisht24 <aniketbisht24@gmail.com>
2022-09-20 21:55:08 +0530

    test client

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import Announcement from './components/Announcement';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Slider from './components/Slider';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+// import Login from './pages/Login';
+// import Product from './pages/Product';
+// import ProductList from './pages/ProductList';
+// import Register from './pages/Register';
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      {/* <Navbar />
+      <Announcement />
+      <Slider />
+      <Footer /> */}
+      {/* <Product /> */}
+      {/* <Register /> */}
+      {/* <Login /> */}
+      {/* <Cart /> */}
+      {/* <ProductList /> */}
+    </div>
+  )
+}
+
+export default App
