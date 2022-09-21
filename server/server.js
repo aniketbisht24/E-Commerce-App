b1dfc757110dc65e8371dd905b9356df0519e845
aniketbisht24 <aniketbisht24@gmail.com>
2022-09-21 06:40:45 +0530

    server-> db connection successful

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+const app = express()
+
+dotenv.config()
+
+mongoose.connect(
+    process.env.MONGO_URL
+    ).then(() => console.log("connection successful")).catch((err) => console.log(err))
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("listening at port 3000")
+})
+
